1c33e636400fce4c4bd7a1cffb4b031d3605229f
albertbrook <3226521643@qq.com>
2020-03-06 21:34:02 +0800

    add Program.cs

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel;
+using System.Windows.Forms;
+
+namespace TowerOfHanoi
+{
+    public class Program : Form
+    {
+        private readonly IContainer components = null;
+
+        private Program()
+        {
+            Text = "Tower Of Hanoi - AlbertBrook";
+            ClientSize = Settings.ScreenSize;
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            MaximizeBox = false;
+            StartPosition = FormStartPosition.CenterScreen;
+            BackColor = Settings.ScreenColor;
+
+            Controls.Add(Picture.GetPicture());
+
+            Functions.GetFunctions();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+                components.Dispose();
+            base.Dispose(disposing);
+        }
+
+        public static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Program());
+        }
+    }
+}
