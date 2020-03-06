4f717cb045d595712e8f8e016c33ded37a06745b
albertbrook <3226521643@qq.com>
2020-03-06 21:34:07 +0800

    add Settings.cs

---
+++ Settings.cs
@@ -0,0 +1,40 @@
+﻿using System.Drawing;
+
+namespace TowerOfHanoi
+{
+    internal class Settings
+    {
+        internal static Size ScreenSize { get; set; }
+        internal static Color ScreenColor { get; set; }
+
+        internal static Size PillarSize { get; set; }
+        internal static Color PillarColor { get; set; }
+
+        internal static int BlockCount { get; set; }
+        internal static Color BlockColor { get; set; }
+        internal static int[] BlockWidths { get; set; }
+        internal static int BlockHeight { get; set; }
+
+        static Settings()
+        {
+            ScreenSize = new Size(1000, 600);
+            ScreenColor = Color.Black;
+            PillarSize = new Size(30, 500);
+            PillarColor = Color.White;
+            BlockCount = 5;
+            BlockColor = Color.White;
+            BlockWidths = SetBlockWidths();
+            BlockHeight = 50;
+        }
+
+        private Settings() { }
+
+        private static int[] SetBlockWidths()
+        {
+            int[] blockWidths = new int[BlockCount];
+            for (int i = 0; i < BlockCount; i++)
+                blockWidths[i] = PillarSize.Width + 40 * (i + 1);
+            return blockWidths;
+        }
+    }
+}
